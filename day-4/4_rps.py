c100c832546d837c9e9c982aab3edfee8b847709
Raj Sekhar Roy <rajsekhar.roy@outlook.com>
2022-07-29 12:23:10 +0530

    Added loop to the RPS game.
    Added loop to the RPS game.
    
    
    
    Signed-off-by: Raj Sekhar Roy <rajsekhar.roy@outlook.com>

--- day-4/4_rps.py
+++ day-4/4_rps.py
@@ -35,39 +35,64 @@ choices = {
     "S": ("Scissors", scissors)  
 }
 
-your_choice= input("Rock! Paper! Scissors! Type 'R' for rock, 'P' for paper and 'S' for scissors ").upper()
-computer_choice = random.choice(options)
-result = ""
+you_win_count =0
+computer_win_count =0
+draw_count = 0
 
-if your_choice == options[0]:
-    if computer_choice == options[0]:
-        result  = "It's a draw."
-    elif computer_choice == options[1]:
-        result = "Computer Wins!"
+while True:
+    print("Press X to quit!")
+    your_choice= input("Rock! Paper! Scissors! Type 'R' for rock, 'P' for paper and 'S' for scissors. ").upper()
+    computer_choice = random.choice(options)
+    result = ""
+
+    if your_choice == "X":
+        break
+
+    if your_choice == options[0]:
+        if computer_choice == options[0]:
+            result  = "It's a draw."
+            draw_count += 1
+        elif computer_choice == options[1]:
+            result = "Computer Wins!"
+            computer_win_count += 1
+        else:
+            result = "You Win!"
+            you_win_count += 1
+    elif your_choice == options[1]:
+        if computer_choice == options[1]:
+            result  = "It's a draw."
+            draw_count += 1
+        elif computer_choice == options[2]:
+            result = "Computer Wins!"
+            computer_win_count += 1
+        else:
+            result = "You Win!"
+            you_win_count += 1
+    elif your_choice == options[2]:
+        if computer_choice == options[2]:
+            result  = "It's a draw."
+            draw_count += 1
+        elif computer_choice == options[0]:
+            result = "Computer Wins!"
+            computer_win_count += 1
+        else:
+            result = "You Win!"
+            you_win_count += 1
     else:
-        result = "You Win!"
-elif your_choice == options[1]:
-    if computer_choice == options[1]:
-        result  = "It's a draw."
-    elif computer_choice == options[2]:
-        result = "Computer Wins!"
-    else:
-        result = "You Win!"
-elif your_choice == options[2]:
-    if computer_choice == options[2]:
-        result  = "It's a draw."
-    elif computer_choice == options[0]:
-        result = "Computer Wins!"
-    else:
-        result = "You Win!"
-else:
-    result = "Invalid choice"
+        result = "Invalid choice. Computer Wins!"
+        computer_win_count += 1
 
 
-print(f"You chose: {choices[your_choice][0]}\n")
-print(f"{choices[your_choice][1]}\n\n")
 
-print(f"Computer chose: {choices[computer_choice][0]}\n")
-print(f"{choices[computer_choice][1]}\n\n")
+    print(f"You chose: {choices[your_choice][0]}\n")
+    print(f"{choices[your_choice][1]}\n\n")
 
-print(f"Result: {result}")
+    print(f"Computer chose: {choices[computer_choice][0]}\n")
+    print(f"{choices[computer_choice][1]}\n\n")
+
+    print(f"Result: {result}")
+
+
+print(f"You won {you_win_count} times.")
+print(f"Computer won {computer_win_count} times.")
+print(f"Draw {computer_win_count} times.")
